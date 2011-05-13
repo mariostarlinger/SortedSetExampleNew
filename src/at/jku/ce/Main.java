39bfa3a67c5d320d73517f3eea283f0ec29c7b3f
Pascal Hoefler <pascal.hoefler@gmx.at>
2011-05-13 15:18:08 +0200

    <Tag1> Clemens: Logger definiert und main Methode befuellt.

--- src/at/jku/ce/Main.java
+++ src/at/jku/ce/Main.java
@@ -2,10 +2,22 @@ package at.jku.ce;
 
 public class Main {
 
-	/**
-	 * @param args
-	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+	
 	public static void main(String[] args) {
+	
+	Set<DomainObject> objectSet = new HashSet<DomainObject>();
+
+	objectSet.add(new DomainObject("Helga"));
+	objectSet.add(new DomainObject("Walter"));
+	objectSet.add(new DomainObject("Hannah"));
+	objectSet.add(new DomainObject("Paul"));
+	objectSet.add(new DomainObject("Claudia"));
+
+	for (DomainObject domainObject : objectSet) {
+	  LOGGER.debug(domainObject.id);
+	}
+
 	}
 
 }
